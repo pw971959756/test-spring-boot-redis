848901f0c30520671ace2fa5a517db69e71b9d74
pw <pengwei940613@163.com>
2017-07-17 23:59:34 +0800

    Java Redis Demo

---
+++ src/test/java/JavaRedisTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+
+/**
+ * Created by pw on 2017/7/17.
+ */
+public class JavaRedisTest {
+
+
+    @Test
+    public void test1(){
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //check whether server is running or not
+        System.out.println("Server is running: "+jedis.ping());
+    }
+
+
+
+    @Test
+    public void test2(){
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //set the data in redis string
+        jedis.set("tutorial-name", "Redis tutorial");
+        // Get the stored data and print it
+        System.out.println("Stored string in redis:: "+ jedis.get("tutorialname"));
+    }
+
+
+    @Test
+    public void test3(){
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+
+        //store data in redis list
+        jedis.lpush("tutorial-list", "Redis");
+        jedis.lpush("tutorial-list", "Mongodb");
+        jedis.lpush("tutorial-list", "Mysql");
+        // Get the stored data and print it
+        List<String> list = jedis.lrange("tutorial-list", 0 ,5);
+
+        for(int i = 0; i<list.size(); i++) {
+            System.out.println("Stored string in redis:: "+list.get(i));
+        }
+    }
+
+
+}
